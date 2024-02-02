adf427bd8df53af9e973af0f14f79355b0c1284c
CharlyRousseau <charly@mbp-de-charly.home>
2024-02-02 12:51:04 +0100

    Part 4

---
+++ part_4_2.sql
@@ -0,0 +1,28 @@
+-- Active: 1706539774613@@127.0.0.1@32768@app_auth
+CREATE TABLE username_history (
+    username_history_id SERIAL PRIMARY KEY, user_id INTEGER, username_new VARCHAR(255), timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION update_username_history(
+) RETURNS TRIGGER AS 
+$$
+BEGIN
+	-- Insérer l'historique des valeurs de username
+	IF TG_OP = 'INSERT'
+	OR TG_OP = 'UPDATE' THEN
+	INSERT INTO
+	    username_history (user_id, username_new)
+	VALUES (NEW.user_id, NEW.username);
+END
+	IF;
+	RETURN NEW;
+END;
+$$
+LANGUAGE
+plpgsql; 
+
+CREATE TRIGGER trigger_update_username
+AFTER INSERT
+OR
+UPDATE ON user_table FOR EACH ROW
+EXECUTE FUNCTION update_username_history ();
